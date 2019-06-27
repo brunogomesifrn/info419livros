679b47e650d8a765b2000bf5a2c63dde370b5ce7
Eduarda <eduardaalcantara2011@live.com>
2019-06-27 11:37:42 -0300

    adicionei novamente o import produtos

--- info419livros/urls.py
+++ info419livros/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import inicial, cadastrar, login, generos
+from core.views import inicial, cadastrar, login, generos, produtos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
