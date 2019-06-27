4e0703e3ebb3336bcad41ee9465aa66b49e529c6
Eduarda <eduardaalcantara2011@live.com>
2019-06-27 09:59:58 -0300

    ajeitei os links de login e cadastro

--- core/views.py
+++ core/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def inicial(request):
 	return render(request, 'index.html')
+
+def cadastrar(request):
+	return render(request, 'register.html')
+
+def login(request):
+	return render(request, 'login.html')
--- info419livros/urls.py
+++ info419livros/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import inicial
+from core.views import inicial, cadastrar, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicial, name='inicial'),
+    path('cadastro/', cadastrar, name='cadastro'),
+    path('login/', login, name='login'),
 ]
