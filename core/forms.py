7d4b88aeddeebda0fe74b36649f186c5e2a02c50
Semaias <semaiasrangel@gmail.com>
2019-06-30 14:42:29 -0300

    Troquei generoForm de lugar com livroForm

--- core/forms.py
+++ core/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from django.forms import ModelForm
 from .models import Livro, Genero
 
-class livroForm(ModelForm):
-    class Meta():
-        model = Livro
-        fields = ['Título', 'Autor', 'Preço', 'ISBN', 'Capa']
-
 class generoForm(ModelForm):
     class Meta():
         model = Genero
         fields = ['Gênero']
+
+class livroForm(ModelForm):
+    class Meta():
+        model = Livro
+        fields = ['Título', 'Autor', 'Preço', 'ISBN', 'Capa']
